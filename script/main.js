f501b6a03eae7ec8f358f2abb82b35f4df82f488
NishanBhattarai327 <bhattarainishan327@gmail.com>
2023-04-05 17:49:33 +0545

    Inserting pieces to the game

--- script/main.js
+++ script/main.js
@@ -1,46 +1,53 @@
-console.log("Chess");
-
-// Define the initial state of the chessboard
-var chessboard = [  
-  ["R", "N", "B", "Q", "K", "B", "N", "R"],
-  ["P", "P", "P", "P", "P", "P", "P", "P"],
-  [" ", " ", " ", " ", " ", " ", " ", " "],
-  [" ", " ", " ", " ", " ", " ", " ", " "],
-  [" ", " ", " ", " ", " ", " ", " ", " "],
-  [" ", " ", " ", " ", " ", " ", " ", " "],
-  ["p", "p", "p", "p", "p", "p", "p", "p"],
-  ["r", "n", "b", "q", "k", "b", "n", "r"]
+// Define the chessboard and piece objects
+const board = [  
+  ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+  ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+  [' ', '.', ' ', '.', ' ', '.', ' ', '.'],
+  ['.', ' ', '.', ' ', '.', ' ', '.', ' '],
+  [' ', '.', ' ', '.', ' ', '.', ' ', '.'],
+  ['.', ' ', '.', ' ', '.', ' ', '.', ' '],
+  ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+  ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
 ];
 
-// Define a function to render the chessboard
-function renderChessboard() {
-  var board = document.getElementById("chessboard");
-  for (var i = 0; i < 8; i++) {
-    var row = board.rows[i];
-    for (var j = 0; j < 8; j++) {
-      var cell = row.cells[j];
-      // Add the appropriate CSS class to style the square
-      if ((i + j) % 2 === 0) {
-        cell.classList.add("white");
-      } else {
-        cell.classList.add("black");
-      }
-      // Add the appropriate CSS class to style the chess piece
-      var piece = chessboard[i][j];
-      if (piece !== " ") {
-        var pieceElement = document.createElement("div");
-        pieceElement.innerHTML = piece;
-        pieceElement.classList.add("piece");
-        if (piece === piece.toUpperCase()) {
-          pieceElement.classList.add("white-piece");
-        } else {
-          pieceElement.classList.add("black-piece");
-        }
-        cell.appendChild(pieceElement);
-      }
+const pieces = {
+  'r': 'white_rook',
+  'n': 'white_knight',
+  'b': 'white_bishop',
+  'q': 'white_queen',
+  'k': 'white_king',
+  'p': 'white_pawn',
+  'R': 'black_rook',
+  'N': 'black_knight',
+  'B': 'black_bishop',
+  'Q': 'black_queen',
+  'K': 'black_king',
+  'P': 'black_pawn'
+};
+
+// Create the chessboard squares and pieces using JavaScript
+const chessboard = document.querySelector('.board');
+let squareColor = "";
+for (let row = 0; row < 8; row++) {
+  for (let col = 0; col < 8; col++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    if ((row + col) % 2 === 0) {
+      square.classList.add('white');
+      squareColor = 'white';
+    } else {
+      square.classList.add('black');
+      squareColor = 'black';
     }
+    const piece = document.createElement('img');
+    const pieceType = pieces[board[row][col]];
+    if (pieceType) {
+      piece.classList.add('piece', board[row][col].toLowerCase());
+      piece.setAttribute('src', `imgs/${pieceType}.png`);
+      piece.setAttribute('alt', 'pieces');
+    }
+    square.appendChild(piece);
+    chessboard.appendChild(square);
   }
 }
 
-// Call the renderChessboard function to initialize the chessboard
-renderChessboard();
